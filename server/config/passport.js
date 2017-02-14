1a48302c179d5fa359965ec524126cb05bd2a26b
Andres Villa <andresfvilla88@gmail.com>
2017-02-13 20:33:55 -0500

    removed password on return

--- server/config/passport.js
+++ server/config/passport.js
@@ -108,6 +108,7 @@ module.exports = function(passport) {
                 return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
 
             // all is well, return successful user
+            delete user.local.password
             return done(null, user);
         });
 
