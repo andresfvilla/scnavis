93bcdc8ffecff444ca8e4b5bd616534922f51181
Andres Villa <andresfvilla88@gmail.com>
2017-02-21 17:05:53 -0500

    renamed app to client

--- client/routes.js
+++ client/routes.js
@@ -2,18 +2,17 @@ import React from 'react';
 import {Route} from 'react-router';
 import App from './components/App';
 import Home from './components/Home';
-import AddCharacter from './components/AddCharacter';
+import Stats from './components/Stats';
 import Character from './components/Character';
 import CharacterList from './components/CharacterList';
-import Stats from './components/Stats';
+import AddCharacter from './components/AddCharacter';
 
 export default (
   <Route component={App}>
     <Route path='/' component={Home} />
-    <Route path='/add' component={AddCharacter} />
-    <Route path='/characters/:id' component={Character} />
-    <Route path='/shame' component={CharacterList} />
     <Route path='/stats' component={Stats} />
+    <Route path='/characters/:id' component={Character} />
+    <Route path='/add' component={AddCharacter} />
     <Route path=':category' component={CharacterList}>
       <Route path=':race' component={CharacterList}>
         <Route path=':bloodline' component={CharacterList} />
